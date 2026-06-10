b19badfd885d32aa3f497628ef108809bda0e3a6
Otto303 <ottodebrie@gmail.com>
2026-06-10 15:41:27 +0200

    remove annotast

--- src/analyzer/absinterpreter.ml
+++ src/analyzer/absinterpreter.ml
@@ -222,7 +222,7 @@ module Make (D : D) = struct
         let arr_annot = read_lvalue state lv' in
         let idx_annot = analyze_expr arr_annot.l_state idx in
         let subscript = Value.array_get (Value.cast_array arr_annot.l_loc arr_annot.l_value) (Value.cast_int idx_annot.e_loc idx_annot.e_value) in
-        Annotast.build_lval lv.l_loc (AArray (arr_annot, idx_annot)) idx_annot.e_state subscript
+        build_lval lv.l_loc (AArray (arr_annot, idx_annot)) idx_annot.e_state subscript
 
   (* Evaluates an lvalue to perform a write operation.
      - If the lvalue is a variable, updates its value in the current state.
